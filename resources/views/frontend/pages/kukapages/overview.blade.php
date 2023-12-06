8cd0d2e72f4008a4265c731b1df1d7174cab8e00
Sazeduzzaman <sazeduzzaman@yahoo.com>
2023-12-06 18:17:10 +0600

    Responsive Done Home

--- resources/views/frontend/pages/kukapages/overview.blade.php
+++ resources/views/frontend/pages/kukapages/overview.blade.php
@@ -1,97 +1,15 @@
 @extends('frontend.master')
 @section('content')
-    <style>
-        /* Need Here */
-        .slick-slider .element {
-            height: 480px;
-            background-color: #000;
-            color: #fff;
-            border-radius: 5px;
-            display: inline-block;
-            margin: 0px 10px;
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: flex;
-            -webkit-box-pack: center;
-            -ms-flex-pack: center;
-            justify-content: center;
-            -webkit-box-align: center;
-            -ms-flex-align: center;
-            align-items: center;
-            font-size: 20px;
-        }
-
-        .slick-slider .slick-disabled {
-            opacity: 0;
-            pointer-events: none;
-        }
-
-
-        .img-fluid {
-            box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-        }
-
-
-        .row-title {
-            color: #555;
-            font-size: 32px;
-        }
-
-
-        .para_text {
-            text-align: justify !important;
-        }
-
-        /* For NEw */
-        .eyebrow {
-            font-size: 22px;
-            font-weight: bold;
-            letter-spacing: 0.44px;
-            background-image: -webkit-gradient(linear, left top, right top, color-stop(4%, #4e4fa9), to(#a831d6));
-            background-image: -webkit-linear-gradient(left, #4e4fa9 4%, #a831d6 100%);
-            background-image: -o-linear-gradient(left, #4e4fa9 4%, #a831d6 100%);
-            background-image: linear-gradient(to right, #4e4fa9 4%, #a831d6 100%);
-            position: relative;
-            text-transform: uppercase;
-            -webkit-background-clip: text;
-            -webkit-text-fill-color: transparent;
-            display: inline-block;
-            margin-bottom: 0.6rem;
-            padding-bottom: 5px;
-        }
-
-
-        .eyebrow:after {
-            content: '';
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            background-color: #4e4fa9;
-            width: calc(100% + 10px);
-            height: 4px;
-            border-radius: 5px;
-        }
-
-        .eyebrow_title {
-            font-family: "Allumi Std Extended";
-            /* font-family: "Klinic Slab"; */
-            text-align: start;
-        }
-
-        /* Need Here End*/
-    </style>
-
     @include('frontend.pages.kukapages.partial.page_header')
     <section class="header" id="myHeader">
         <div class="brand-page-content container nav-bar">
-            <div class=" px-5 pb-0 pt-0">
+            <div class="px-5 brand-p-content">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h3 class="mb-3" style="color:#555;">{{ ucfirst($brand->title) }}</h3>
+                        <h3 class="mb-3 brand-p-title d-lg-block d-sm-none" style="color:#555;">{{ ucfirst($brand->title) }}</h3>
                         @if (!empty($brandpage->row_six_title))
                             <h5 class="company-tab-title mb-2 border-bottom">
-                                <span class="rounded-pill"
-                                    style="font-size: 25px; background-color: #ffffff;">{{ $brandpage->row_six_title }}</span>
+                                <span class="rounded-pill text-black bg-white">{{ $brandpage->row_six_title }}</span>
                             </h5>
                             <p class="mt-4 text-justify-align" style="font-size: 16px;">
                                 {!! $brandpage->row_six_header !!}
@@ -100,12 +18,13 @@
                     </div>
                 </div>
                 @if (!empty($row_one))
-                    <div class="row my-5 no-margin">
+                    <div class="row no-margin">
                         {{-- New One --}}
-                        <div class="col-lg-6 col-sm-12 ">
+                        <div class="col-lg-6 col-sm-12 p-0">
                             <div class="d-flex justify-content-center">
-                                <img class="img-fluid" src="{{ (!empty($row_one->image) && file_exists(public_path('storage/' . $row_one->image))) ? asset('storage/' . $row_one->image) : asset('frontend/images/no-row-img(580-326).png') }}" alt=""
-                                    style="border-radius: 7px 55px 7px 55px;">
+                                <img class="img-fluid"
+                                    src="{{ !empty($row_one->image) && file_exists(public_path('storage/' . $row_one->image)) ? asset('storage/' . $row_one->image) : asset('frontend/images/no-row-img(580-326).png') }}"
+                                    alt="" style="border-radius: 7px 55px 7px 55px;">
                                 {{-- @php
                                     $imagePath = !empty($row_one->image) && file_exists(public_path('storage/' . $row_one->image)) ? asset('storage/' . $row_one->image) : asset('frontend/images/no-row-img(580-326).png');
                                 @endphp
@@ -151,7 +70,7 @@
                         </div>
                     </div>
                 @endif
-                <div class="my-5">
+                <div class="">
                     <div class="container">
                         <!-- section title -->
                         <div class="col-lg-10 offset-1">
@@ -207,7 +126,7 @@
             @endif
             <div class="p-5 pb-0 pt-0">
                 @if (!empty($row_four))
-                    <section class="py-4 my-5">
+                    <section class="">
                         <div class="container">
                             <div class="row">
                                 <div class="col-lg-6 col-sm-12 para_text">
@@ -234,7 +153,7 @@
                     </section>
                 @endif
                 @if (!empty($row_five))
-                    <section class="my-5">
+                    <section class="">
                         <div class="container">
                             <div class="row">
                                 <div class="col-lg-6 col-sm-12 para_text">
--- resources/views/frontend/pages/kukapages/partial/page_header.blade.php
+++ resources/views/frontend/pages/kukapages/partial/page_header.blade.php
@@ -1,65 +1,3 @@
-<style>
-    .page-wrapper{
-        margin-top: -3.2rem;
-    }
-    .main-header {
-        position: relative;
-    }
-
-    .drpdown_menu {
-        z-index: 1021;
-    }
-
-
-    body {
-        background-color: #f6f6f6;
-    }
-
-    @media (min-width: 1300px) {
-
-        .container,
-        .container-lg,
-        .container-md,
-        .container-sm,
-        .container-xl {
-            max-width: 1250px;
-        }
-    }
-
-    @media (min-width: 1400px) {
-
-        .container,
-        .container-lg,
-        .container-md,
-        .container-sm,
-        .container-xl {
-            max-width: 1350px;
-        }
-    }
-
-    @media (min-width: 1500px) {
-
-        .container,
-        .container-lg,
-        .container-md,
-        .container-sm,
-        .container-xl {
-            max-width: 1450px;
-        }
-    }
-</style>
-<style>
-    .sticky {
-        position: fixed;
-        top: 0;
-        width: 100%;
-        z-index: 9999;
-    }
-
-    .sticky+.content {
-        padding-top: 102px;
-    }
-</style>
 <section>
     <div class="brand-page-banner">
         {{-- <div class="row">
@@ -67,12 +5,10 @@
                 <h3 class="text-center text-white">{{ $brandpage->header }}</h3>
             </div>
         </div> --}}
-        {{-- <img src="{{ !empty($brandpage->banner_image) && file_exists(asset('storage/' . $brandpage->banner_image)) ? asset('storage/' . $brandpage->banner_image) : asset('frontend/images/no-banner(1920-330).png') }}"
-            alt=""> --}}
-        <img src="{{ asset('storage/' . $brandpage->banner_image)}}"
+        <img src="{{ !empty($brandpage->banner_image) && file_exists(public_path('storage/' . $brandpage->banner_image)) ? asset('storage/' . $brandpage->banner_image) : asset('frontend/images/no-banner(1920-330).png') }}"
             alt="">
     </div>
-    <div class="container d-lg-block d-sm-none">
+    {{-- <div class="container d-lg-block d-sm-none">
         <div class="row d-lg-flex align-items-center">
             <div class="col pt-1">
                 <ul class="d-lg-flex align-items-center brand-bread-crumb my-2">
@@ -92,28 +28,23 @@
                 </ul>
             </div>
         </div>
-    </div>
+    </div> --}}
 </section>
-<section class="header" id="myHeader">
+<section class="header d-lg-block d-sm-none" id="myHeader">
     <div class="brand-page-header-container container">
         <div class="row d-lg-flex align-items-center">
             <div class="col-lg-2 col-sm-6 me-3 extra-d-flex">
-                {{-- <img id="stand-logo" src="{{ asset('storage/' . $brandpage->brand_logo) }}" alt="{{ $brand->title }} - logo"
-                    height="58px"> --}}
-                <img id="stand-logo" class="img-fluid" height=""
+                <div>
+                    <img id="stand-logo" class="img-fluid" height=""
+                        src="http://ngenitltd.com/storage/BbXjqfGfxBEJmLGq1697946011.png"
+                        alt="{{ $brand->title }} - logo">
+                </div>
+                {{-- <img id="stand-logo" class="img-fluid" height=""
                     src="{{asset('storage/' . $brandpage->brand_logo)}}"
-                    alt="{{ $brand->title }} - logo" style="box-shadow: none;">
-                {{-- <img id="stand-logo" class="img-fluid"
-                    src="{{ isset($brandpage->brand_logo) && file_exists(asset('storage/' . $brandpage->brand_logo))
-                        ? asset('storage/' . $brandpage->brand_logo)
-                        : asset('frontend/images/no-brand-logo.jpg') }}"
                     alt="{{ $brand->title }} - logo" style="box-shadow: none;"> --}}
             </div>
             <div class="col-lg-1 col-sm-6 extra-d-flex">
                 <a href="#" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                    {{-- <img class="stand-header-icons video-corporate"
-                        src="https://img.directindustry.com/media/ps/images/common/stand/video-icon.gif"
-                        alt="video corpo"> --}}
                     <span class="text-muted">No video Available</span>
                 </a>
                 {{-- <!-- Modal -->
@@ -135,7 +66,7 @@
                 </div>
                 <!-- Modal End--> --}}
             </div>
-            <div class="col-lg-8 col-sm-12 extra-d-flex">
+            <div class="col-lg-8 col-sm-12">
                 <div class="common_button2 d-lg-none d-sm-block">
                     <a href="javascript:void(0)" class="dropdown-toggle w-100 text-white" type="button" data-bs-toggle="dropdown"
                         data-bs-auto-close="outside" aria-expanded="false">
@@ -199,6 +130,72 @@
         </div>
     </div>
 </section>
+
+
+<section class="header d-lg-none d-sm-block" id="myHeader">
+    <div class="brand-page-header-container container">
+        <div class="row d-lg-flex align-items-center">
+            <div class="col-lg-12">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <img id="stand-logo" class="img-fluid" height=""
+                            src="http://ngenitltd.com/storage/BbXjqfGfxBEJmLGq1697946011.png"
+                            alt="{{ $brand->title }} - logo">
+                    </div>
+                    <div>
+                        <a href="#" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            <span class="text-muted">No video Available</span>
+                        </a>
+                        {{-- <!-- Modal -->
+                        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                            tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered modal-lg">
+                                <div class="modal-content">
+                                    <div class="modal-header rounded-0 p-1 px-3 m-0" style="background-color: #ae0a46;">
+                                        <h5 class="modal-title text-white" id="staticBackdropLabel">Video</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <iframe src="https://www.youtube.com/embed/4z2PyfaoiYk?rel=0&amp;showinfo=0"
+                                            frameborder="0" allowfullscreen="" style="width: 100%; height: 350px;"></iframe>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Modal End--> --}}
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <ul class="d-flex align-items-center justify-content-center">
+                            <li class="px-3">
+                                <a class="{{ Route::current()->getName() == 'brand.overview' ? 'active-brands' : '' }}"
+                                    href="{{ route('brand.overview', $brand->slug) }}">Company</a>
+                            </li>
+                            <li class="px-3">
+                                <a class="{{ in_array(Route::currentRouteName(), ['brand.products', 'product.details']) ? 'active-brands' : '' }}"
+                                    href="{{ route('brand.products', $brand->slug) }}">Products</a>
+                            </li>
+    
+    
+                            <li class="px-3">
+                                <a class="{{ Route::current()->getName() == 'brand.pdf' ? 'active-brands' : '' }}"
+                                    href="{{ route('brand.pdf', $brand->slug) }}">Catalogs</a>
+                            </li>
+    
+                            <li class="px-3">
+                                <a class="{{ Route::current()->getName() == 'brand.content' ? 'active-brands' : '' }}"
+                                    href="{{ route('brand.content', $brand->slug) }}">Contents</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 <script>
     document.addEventListener("DOMContentLoaded", function() {
         window.onscroll = function() {
