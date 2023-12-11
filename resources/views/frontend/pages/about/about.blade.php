de2b4237ccca3a60a73ac9827cf1878a77d29039
Sazeduzzaman <sazeduzzaman@yahoo.com>
2023-12-11 17:53:04 +0600

    about-us,

--- resources/views/frontend/pages/about/about.blade.php
+++ resources/views/frontend/pages/about/about.blade.php
@@ -20,11 +20,13 @@
     @if (!empty($row1))
         <section>
             <div class="container">
-                <div class="row py-3">
-                    <h3 class="text-center w-50 mx-auto">
-                        {{ $row1->banner_short_description }}
-                    </h3>
-                </div>
+                @if (!empty($row1->banner_short_description)) 
+                    <div class="row py-3">
+                        <h3 class="text-center w-50 mx-auto">
+                            {{ $row1->banner_short_description}}
+                        </h3>
+                    </div>
+                @endif
                 <div class="row d-flex align-items-center mt-4">
                     <span class="text-start pt-3 ps-0 mb-3 d-flex align-items-center ms-2"
                         style="border-bottom: 2px solid #ae0a46; font-size: 20px;">
@@ -36,8 +38,8 @@
                         <img class="img-fluid " src="{{ asset('storage/' . $row1->image) }}" alt="">
                     </div> --}}
                     <div class="col-lg-12">
-                        <div class="">
-                            <h2>{{ $row1->title }}</h2>
+                        <div class="about-column-first">
+                            <h2 class="about-title">{{ $row1->title }}</h2>
                             <p> {!! $row1->description !!} </p>
                             @if (!empty($row1->btn_name))
                                 <div class="btn_left">
@@ -55,7 +57,7 @@
         <section>
             <div class="container">
                 <div class="row d-flex align-items-center">
-                    <span class="text-start pt-3 ps-0 mb-3 d-flex align-items-center justify-content-start ms-2"
+                    <span class="pt-3 ps-0 mb-3 d-flex align-items-center justify-content-start ms-2"
                         style="border-bottom: 2px solid #ae0a46; font-size: 20px;">
                         <div
                             style="background: #ae0a46;
@@ -65,8 +67,8 @@
                         </div>
                     </span>
                     <div class="col-lg-12">
-                        <div class="text-start">
-                            <h2>{{ $row2->title }}</h2>
+                        <div class="about-column-first">
+                            <h2 class="about-title">{{ $row2->title }}</h2>
                             <p> {!! $row2->description !!} </p>
                             @if (!empty($row2->btn_name))
                                 <div class="btn_left">
@@ -75,18 +77,14 @@
                             @endif
                         </div>
                     </div>
-                    {{-- <div class="col-lg-6 mt-3 d-flex justify-content-center">
-                        <img class="img-fluid" src="{{ asset('storage/' . $row2->image) }}" alt=""
-                            style="width: 360px;">
-                    </div> --}}
                 </div>
             </div>
         </section>
     @endif
 
     <!--======// CEO Details Section //======-->
-    {{-- <section>
-        <div class="call_to_action" style="background-image: url(images/hardware/calltoaction.jpg);">
+    <section>
+        <div class="call_to_action d-lg-block d-sm-none" style="background-image: url(images/hardware/calltoaction.jpg);">
             <div class="container">
                 <!-- about seo -->
                 <div class="about_seo_wrapper">
@@ -100,7 +98,7 @@
                             <h4 class="text-black">Our goal at NGen IT is to make meaningful connections that positively
                                 impact the lives of
                                 the people we serve, including our clients, partners and teammates.</h4>
-                            <span class="text-black">Md Rasel sir</span> <br>
+                            <span class="text-black">Your Name</span> <br>
                             <span class="text-black fw-bold">CEO NGenit</span>
                             <div class="pt-5">
                                 <a href="" class="product_button">Meet our leadership</a>
@@ -110,9 +108,9 @@
                 </div>
             </div>
         </div>
-    </section> --}}
+    </section>
     @if (!empty($about->video_link))
-        <section class="container mt-3 mb-5">
+        <section class="container">
             <div class="software_feature_title pb-5">
                 <h1 class="text-center ">{{ $about->row_four_title }}</h1>
             </div>
@@ -125,10 +123,10 @@
                 </div>
                 <div class="col-lg-6 col-sm-6">
                     <div class="home_title">
-                        <h5 class="home_title_heading" style="text-align: left;">
+                        <h5 class="home_title_heading">
                             {{ $about->video_row_title }}
                         </h5>
-                        <p class="home_title_text" style="text-align: left;">
+                        <p class="home_title_text pt-4">
                             {{ $about->video_row_short_description }}</p>
                         {{-- <div class="business_seftion_button text-center">
                             <a class="common_button2"
@@ -143,26 +141,26 @@
         <section>
             <div class="container py-5">
                 <div class="row d-flex align-items-center gx-1">
-                    <span class="text-start pt-3 ps-0 mb-3 d-flex align-items-center justify-content-start ms-2"
+                    <span class="d-flex justify-center"
                         style="border-bottom: 2px solid #ae0a46; font-size: 20px;">
                         <div style="background:#ae0a46;color:white;padding: 10px; display: flex; align-items: center;">
                             <span>{{ $row3->badge }}</span>
                         </div>
                     </span>
                     <div class="col-lg-10">
-                        <div class="text-start">
-                            <h2>{{ $row3->title }}</h2>
+                        <div class="text-start about_seo_text">
+                            <h2 class="">{{ $row3->title }}</h2>
                             <p>{!! $row3->description !!}</p>
                         </div>
                     </div>
                     <div class="col-lg-2 text-center">
                         <div class="container">
                             <div class="row">
-                                <h4 class="mb-3">All Profiles</h4>
+                                <h4 class="mb-3"><span style="border-top: 3px solid #ae0a46;">All</span> Profiles</h4>
                                 @foreach ($pdfs as $pdf)
                                     <div class="col-lg-12 mb-2">
                                         <a href="{{ asset('storage/files/' . $pdf->document) }}" type="button"
-                                            class="effect02 pdf_btn me-2">
+                                            class="btn-white pdf_btn me-2">
                                             <span class="btn-label"><i class="ph-download-simple fw-bolder me-2"></i></i>
                                                 {{ $pdf->button_name }}
                                             </span>
