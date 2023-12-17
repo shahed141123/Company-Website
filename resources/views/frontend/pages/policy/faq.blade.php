27774b7e3b48a8f8b321ae1ac381852c80132a32
Sazeduzzaman <sazeduzzaman@yahoo.com>
2023-12-17 11:39:59 +0600

    updated

--- resources/views/frontend/pages/policy/faq.blade.php
+++ resources/views/frontend/pages/policy/faq.blade.php
@@ -1,28 +1,7 @@
 @extends('frontend.master')
 @section('content')
-    <section class="common_product_header"
-        style="background-image: linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)),url('https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2020/03/11140107/find-remote-job-1024x512.png');">
-        <div class="container ">
-            <h1>FAQ</h1>
-            <div class="row">
-                <div class="input-group w-50 mx-auto">
-                    <input type="text" class="form-control" placeholder="Search">
-                    <div class="input-group-append">
-                        <button class="btn job_search_btn" type="button">
-                            <i class="fa fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-                <!--BUTTON START-->
-                <div class="d-flex justify-content-center align-items-center">
-                    <div class="m-4">
-                        <a class="common_button2" href="{{ route('contact') }}">Talk to a specialist</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    {{--  --}}
+
+
     <!-- Demo header-->
     <section class="header mb-4">
         <div class="container py-4">
