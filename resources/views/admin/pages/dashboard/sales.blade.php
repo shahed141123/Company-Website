acd739445819e76ebfe73d86a39d493dcf08f856
Sazeduzzaman <sazeduzzaman@yahoo.com>
2024-01-31 15:37:57 +0600

    dropoff page almost done

--- resources/views/admin/pages/dashboard/sales.blade.php
+++ resources/views/admin/pages/dashboard/sales.blade.php
@@ -9,10 +9,10 @@
                 <div class="page-header-content d-lg-flex ">
                     <div class="d-flex px-2">
                         <div class="breadcrumb py-2">
+                            <!-- Breadcrumb Links -->
                             <a href="{{ route('admin.dashboard') }}" class="breadcrumb-item"><i class="ph-house"></i></a>
                             <a href="{{ route('admin.dashboard') }}" class="breadcrumb-item">Home</a>
-                            <a href="" class="breadcrumb-item"><span class="breadcrumb-item active">Sales
-                                    Dashboard</span></a>
+                            <a href="" class="breadcrumb-item"><span class="breadcrumb-item active">Sales Dashboard</span></a>
                         </div>
                         <a href="#breadcrumb_elements"
                             class="btn btn-light align-self-center collapsed d-lg-none border-transparent rounded-pill p-0 ms-auto"
@@ -29,116 +29,118 @@
         <!-- Sales Chain Page -->
         <div class="content pt-0">
             <div class="container-fluid ">
-                <div class="row">
+                <div class="row mt-2 mb-5">
                     <h1 class="mb-0 text-center w-25 mx-auto mb-2 text-info"
                         style="color: #247297; border-bottom: 1px solid #247297;">Sales Dashboard</h1>
                 </div>
                 {{-- Extra Area --}}
                 <div class="row">
+                    <!-- Target Vs. Achievement Section -->
                     <div class="col-lg-4">
                         <h6 style="color: #247297; border-bottom: 1px solid #247297;" class=" mb-0 pt-2 text-center">
-                            Target
-                            Vs. Achievement</h6>
-                        <div class="card card-body border-0 shadow-none mb-0"
-                            style="border-top-left-radius: 0px;border-top-right-radius: 0px;">
+                            Target Vs. Achievement
+                        </h6>
+                        <div class="card card-body border-0 rounded-0 mb-0 "
+                            style="border-top-left-radius: 0px;border-top-right-radius: 0px; box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px !important;">
                             <div class="">
                                 <div class="row">
-                                    <div class="col-sm-12 bg-light px-1 py-1 rounded">
+                                    <!-- Year Target -->
+                                    <div class="col-sm-12 bg-light px-1 py-1 rounded-0">
                                         <div class="d-flex justify-content-between">
-                                            <a href="{{ route('salesYearTarget.index') }}">
+                                            <a href="{{ route('salesYearTarget.index') }}" class="text-muted">
                                                 Year Target
                                             </a>
-                                            <a href="{{ route('salesYearTarget.create') }}"><span
-                                                    class="rounded-circle border border-secondary text-center">
+                                            <a href="{{ route('salesYearTarget.create') }}">
+                                                <span class="rounded-0 border border-secondary text-center">
                                                     <i class="fa fa-plus text-secondary"></i>
-                                                </span></a>
+                                                </span>
+                                            </a>
                                         </div>
                                     </div>
-                                    <div class="col-sm-12 bg-light px-1 py-1 rounded mt-1">
+                                    <!-- Individual Target -->
+                                    <div class="col-sm-12 bg-light px-1 py-1 rounded-0 mt-1">
                                         <div class="d-flex justify-content-between">
-                                            <a href="{{route('salesTeamTarget.index')}}">
+                                            <a href="{{route('salesTeamTarget.index')}}" class="text-muted">
                                                 Individual Target
                                             </a>
-                                            <a href="{{route('salesTeamTarget.create')}}"><span
-                                                    class="rounded-circle border border-secondary text-center">
+                                            <a href="{{route('salesTeamTarget.create')}}">
+                                                <span class="rounded-0 border border-secondary text-center">
                                                     <i class="fa fa-plus text-secondary"></i>
-                                                </span></a>
+                                                </span>
+                                            </a>
                                         </div>
                                     </div>
-                                    <div class="col-sm-12 bg-light px-1 py-1 rounded mt-1">
+                                    <!-- Achievement Summary -->
+                                    <div class="col-sm-12 bg-light px-1 py-1 rounded-0 mt-1">
                                         <div class="d-flex justify-content-between">
-                                            <a href="{{ route('industry.index') }}">
+                                            <a href="{{ route('industry.index') }}" class="text-muted">
                                                 Achievement Summary
                                             </a>
-                                            <a href=""><span
-                                                    class="rounded-circle border border-secondary text-center">
+                                            <a href=""><span class="rounded-0 border border-secondary text-center">
                                                     <i class="fa fa-plus text-secondary"></i>
-                                                </span></a>
+                                                </span>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <!-- Sales Details Section -->
                     <div class="col-lg-4">
                         <h6 style="color: #247297; border-bottom: 1px solid #247297;" class=" mb-0 pt-2 text-center">
                             Sales Details</h6>
-                        <div class="card card-body border-0 shadow-none mb-0"
-                            style="border-top-left-radius: 0px;border-top-right-radius: 0px;">
+                        <div class="card card-body border-0 mb-0 rounded-0"
+                            style="border-top-left-radius: 0px;border-top-right-radius: 0px; box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px !important;">
                             <div class="row">
-                                <div class="col-sm-12 bg-light px-1 py-1 rounded">
+                                <!-- Sales Profit Loss -->
+                                <div class="col-sm-12 bg-light px-1 py-1 rounded-0">
                                     <div class="d-flex justify-content-between">
-                                        <a href="{{ route('sales-profit-loss.index') }}">
+                                        <a href="{{ route('sales-profit-loss.index') }}" class="text-muted">
                                             Sales Profit Loss
                                         </a>
-                                        <a href="{{ route('sales-profit-loss.create') }}"><span class="rounded-circle border border-secondary text-center">
+                                        <a href="{{ route('sales-profit-loss.create') }}">
+                                            <span class="rounded-0 border border-secondary text-center">
                                                 <i class="fa fa-plus text-secondary"></i>
-                                            </span></a>
+                                            </span>
+                                        </a>
                                     </div>
                                 </div>
-                                <div class="col-sm-12 bg-light px-1 py-1 rounded mt-1">
+                                <!-- Order Status -->
+                                <div class="col-sm-12 bg-light px-1 py-1 rounded-0 mt-1">
                                     <div class="d-flex justify-content-between">
-                                        <a href="{{ route('rfqOrderStatus.index') }}">
+                                        <a href="{{ route('rfqOrderStatus.index') }}" class="text-muted">
                                             Order Status
                                         </a>
-                                        <a href="{{ route('rfqOrderStatus.create') }}"><span class="rounded-circle border border-secondary text-center">
+                                        <a href="{{ route('rfqOrderStatus.create') }}">
+                                            <span class="rounded-0 border border-secondary text-center">
                                                 <i class="fa fa-plus text-secondary"></i>
-                                            </span></a>
-                                    </div>
-                                    {{-- </div>
-                                <div class="col-sm-12 bg-light px-1 py-1 rounded mt-1">
-                                    <div class="d-flex justify-content-between">
-                                        <a href="{{ route('industry.index') }}">
-                                            Achievement Summary
+                                            </span>
                                         </a>
-                                        <a href=""><span
-                                                class="rounded-circle border border-secondary text-center">
-                                                <i class="fa fa-plus text-secondary"></i>
-                                            </span></a>
                                     </div>
-                                </div> --}}
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <!-- RFQ Details Section -->
                     <div class="col-lg-4">
-                        <h6 style="color: #247297; border-bottom: 1px solid #247297;" class=" mb-0 pt-2 text-center">
-                            RFQ Details</h6>
-                        <div class="card card-body border-0 shadow-none mb-0">
+                        <h6 style="color: #247297; border-bottom: 1px solid #247297;" class=" mb-0 pt-2 text-center">RFQ Details</h6>
+                        <div class="card card-body border-0 mb-0 rounded-0" style="box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px !important;">
+                            <!-- Total RFQ & Deal Section -->
                             <div class="d-flex align-items-center mb-3">
                                 <div class="flex-fill title_text_link">
                                     <a href="../full/income.html">
                                         <h6 class="mb-0 text-primary">Total RFQ & Deal</h6>
                                     </a>
-                                    {{-- <span class="text-muted">Until {{ Carbon\Carbon::now()->format('d-m-Y') }}</span> --}}
                                 </div>
                                 <a href="{{ route('rfq-manage.index') }}">
-                                    <button type="button" class="btn  rounded-circle dashboard_btn"
+                                    <button type="button" class="btn  rounded-0 dashboard_btn"
                                         style="width: 30px; height: 30px">
                                         <i style="color: #247297" class="fa-solid fa-badge-percent fs-4 p-1"></i>
                                     </button>
                                 </a>
                             </div>
+                            <!-- Today's and This Month's RFQ & Deal Counts -->
                             <div class="box_details">
                                 <a href="{{ route('rfq-manage.index') }}">
                                     <span class="float-end">RFQ :
@@ -164,7 +166,6 @@
                             </div>
                         </div>
                     </div>
-                    <!-- Sales Chain Page -->
                 </div>
             </div>
         </div>
