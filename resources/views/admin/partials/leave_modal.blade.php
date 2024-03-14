c397d4d1c01b58526920eb864d6e51bd9d27de3c
dev4ngenit <dev4.ngenit@gmail.com>
2024-03-14 10:16:25 +0600

    14-03-2024:shahed-10.16

--- resources/views/admin/partials/leave_modal.blade.php
+++ resources/views/admin/partials/leave_modal.blade.php
@@ -213,3 +213,227 @@
         </div>
     </div>
 </div>
+
+@isset($leaveApplications)
+    @foreach ($leaveApplications as $leaveApplication)
+        <div class="modal fade" id="makeleaveEdit" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1"
+            aria-labelledby="checkapprovedLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+                <div class="modal-content rounded-0">
+                    <div class="modal-header rounded-0 text-white bg-secondary">
+                        <h5 class="modal-title text-uppercase" id="checkapprovedLabel">Leave Application Edit</h5>
+                        <h2 class="accordion-header d-flex align-items-center" id="flush-headingOne">
+                            <button class="ms-2 button-37 collapsed" type="button" data-bs-target="#flush-collapseOne"
+                                aria-expanded="false" aria-controls="flush-collapseOne">
+                                <a href="javascript:void(0);" class="text-primary">
+                                    <i class="fa-solid fa-eyes p-1 rounded-circle" style=" color: #174a62;"></i>
+                                </a>
+                            </button>
+                        </h2>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body rounded-0">
+                        <div class="container">
+                            <div class="row my-2">
+                                <div class="col-lg-12">
+                                    <form class="" action="#">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Applicant Name:
+                                                            <span class="text-danger">*</span></label>
+                                                        <input type="text" name="name"
+                                                            value="{{ $leaveApplication->name }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Enter Applicant Name">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Type Of Leave: <span
+                                                                class="text-danger">*</span></label>
+                                                        <input type="text" name="type_of_leave"
+                                                            value="{{ $leaveApplication->type_of_leave }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Enter Type Of Leave">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Designation: <span
+                                                                class="text-danger">*</span></label>
+                                                        <input type="text" name="designation"
+                                                            value="{{ $leaveApplication->designation }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Enter Your Designation">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Company:</label>
+                                                        <input type="text" name="company"
+                                                            value="{{ $leaveApplication->company }}"
+                                                            class="form-control form-control-sm" placeholder="NGEN IT">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Leave Start Date:
+                                                            <span class="text-danger">*</span></label>
+                                                        <input type="date" name="leave_start_date"
+                                                            value="{{ $leaveApplication->leave_start_date }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Leave Start Date">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Leave End Date:
+                                                            <span class="text-danger">*</span></label>
+                                                        <input type="date" name="leave_end_date"
+                                                            value="{{ $leaveApplication->leave_end_date }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Leave End Date">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Total Days: <span
+                                                                class="text-danger">*</span></label>
+                                                        <input type="text" name="total_days"
+                                                            value="{{ $leaveApplication->total_days }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Enter Total Dayes">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Job Status <span
+                                                                class="text-danger">*</span></label>
+                                                        <input type="text" name="job_status"
+                                                            value="{{ $leaveApplication->job_status }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Enter Job Status">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Reporting On <span
+                                                                class="text-danger">*</span></label>
+                                                        <input type="date" name="reporting_on"
+                                                            value="{{ $leaveApplication->reporting_on }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Leave End Date">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Leave Explanation
+                                                            <span class="text-danger">*</span></label>
+                                                        <textarea name="leave_explanation" class="form-control form-control-sm" id="" cols="30"
+                                                            rows="1" placeholder="Enter Leave Explanation">{{ $leaveApplication->leave_explanation }}</textarea>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Substitute During
+                                                            Leave
+                                                            <span class="text-danger">*</span></label>
+                                                        <input type="text" name="substitute_during_leave"
+                                                            value="{{ $leaveApplication->substitute_during_leave }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Enter Substitute During Leave">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Leave
+                                                            Address</label>
+                                                        <input type="text" name="leave_address"
+                                                            value="{{ $leaveApplication->leave_address }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Leave End Date">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Is Betwwen
+                                                            Holiday</label>
+                                                        <div class="d-flex align-items-center">
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="radio"
+                                                                    name="is_between_holidays" value="yes"
+                                                                    id="yes" @checked($leaveApplication->is_between_holidays == 'yes') disabled>
+                                                                <label class="form-check-label" for="yes">
+                                                                    Yes
+                                                                </label>
+                                                            </div>
+                                                            <div class="form-check ms-3">
+                                                                <input class="form-check-input" type="radio"
+                                                                    name="is_between_holidays" value="no"
+                                                                    id="no" @checked($leaveApplication->is_between_holidays == 'no') disabled>
+                                                                <label class="form-check-label" for="no">
+                                                                    No
+                                                                </label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Leave Contact
+                                                            Number</label>
+                                                        <input type="text" name="leave_contact_no"
+                                                            value="{{ $leaveApplication->leave_contact_no }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Enter Enter Leave Contact Number">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Include Open
+                                                            Saturday</label>
+                                                        <input type="text" name="included_open_saturday"
+                                                            value="{{ $leaveApplication->included_open_saturday }}"
+                                                            class="form-control form-control-sm"
+                                                            placeholder="Enter Include Open Saturday">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Substitute Signature
+                                                            <span class="text-danger">*</span></label>
+                                                        <input type="file" name="substitute_signature"
+                                                            value="{{ $leaveApplication->substitute_signature }}"
+                                                            class="form-control form-control-sm">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="mb-1">
+                                                        <label class="form-label mb-0">Applicant Signature
+                                                            <span class="text-danger">*</span></label>
+                                                        <input type="file" name="applicant_signature"
+                                                            value="{{ $leaveApplication->applicant_signature }}"
+                                                            class="form-control form-control-sm">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card-footer d-flex justify-content-end mt-3">
+                                            {{-- <button type="reset" value="Reset" class="submit_close_btn "
+                                                data-bs-dismiss="modal">Reset Form</button>
+                                            <button type="submit" class="submit_btn from-prevent-multiple-submits"
+                                                style="padding: 4px 9px;">Submit</button> --}}
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+@endisset
