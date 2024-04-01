32379019f46fe9d865da37acee6a7f5de89f7059
dev4ngenit <dev4.ngenit@gmail.com>
2024-04-01 13:11:18 +0600

    31-3-24:Shahed-1.43

--- resources/views/admin/pages/rfq-manage/partial/rfq_table.blade.php
+++ resources/views/admin/pages/rfq-manage/partial/rfq_table.blade.php
@@ -63,123 +63,7 @@
 
 {{-- Modals --}}
 @foreach ($rfqs as $rfq)
-    <div id="show-deals-{{ $deal->rfq_code }}" class="modal fade" tabindex="-1" style="display: none;"
-        aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable modal-lg rounded-0 modal-dialog-centered">
-            <div class="modal-content rounded-0">
-                <div class="modal-header rounded-0">
-                    @php
-                        $rfq_details = App\Models\Admin\Rfq::where('rfq_code', $deal->rfq_code)->first();
-                        $deal_products = App\Models\Admin\DealSas::where('rfq_code', $rfq_details->rfq_code)->get();
-                    @endphp
-                    <h5 class="modal-title">Deal Details : {{ $rfq_details->rfq_code }}</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                </div>
 
-
-                <div class="modal-body border br-7">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <p class="devider-text mb-0 p-1">Client Detils</p>
-                            <div class="border card rounded-0">
-                                <div class="row mt-1">
-                                    <div class="col-lg-12 d-flex justify-content-between">
-                                        <p class="p-2 m-0 text-start">
-                                            <span class="text-secondary">Client Type :</span> <br>
-                                            {{ ucfirst($rfq_details->client_type) }}
-                                        </p>
-                                        <p class="p-2 m-0 text-start">
-                                            <span class="text-secondary">Name :</span> <br>
-                                            {{ ucfirst($rfq_details->name) }}
-                                        </p>
-                                        <p class="p-2 m-0 text-start">
-                                            <span class="text-secondary">Company Name :</span> <br>
-                                            {{ ucfirst($rfq_details->company_name) }}
-                                        </p>
-                                        <p class="p-2 m-0 text-start">
-                                            <span class="text-secondary">Phone Number : </span> <br>
-                                            {{ $rfq_details->phone }}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <p class="devider-text mb-0 p-1">RFQ Details</p>
-                            <div class="border card rounded-0">
-                                <div class="row mt-1 px-0 pt-2">
-                                    <div class="col-lg-12">
-                                        <div class="table-responsive p-2">
-                                            <table class="table table-bordered table-striped p-1">
-                                                <thead
-                                                    style="background-color: #2472979e !important;color: white !important;">
-                                                    @if (count($deal_products) > 0)
-                                                        <tr>
-                                                            <th class="p-1"> Product Name</th>
-                                                            <th class="p-1"> Quantity </th>
-                                                            <th class="p-1"> Sale Price </th>
-                                                        </tr>
-
-                                                        @foreach ($deal_products as $item)
-                                                            <tr class="text-black bg-white">
-                                                                <td>{{ $item->item_name }}</td>
-                                                                <td>{{ $item->qty }}</td>
-                                                                <td>{{ $item->sub_total_cost }}
-                                                                </td>
-                                                            </tr>
-                                                        @endforeach
-                                                    @else
-                                                    @endif
-                                                </thead>
-                                            </table>
-                                        </div>
-                                        <div class="row py-3 px-3">
-                                            <div class="col-lg-4 text-start">
-                                                <p class="p-0 m-0 text-secondary">Asking Quantity : </p> bt
-                                                @if (App\Models\Admin\DealSas::where('rfq_id', $rfq_details->id)->sum('qty') > 0)
-                                                    {{ App\Models\Admin\DealSas::where('rfq_id', $rfq_details->id)->sum('qty') }}
-                                                @else
-                                                    {{ $rfq_details->qty }}
-                                                @endif
-                                            </div>
-                                            <div class="col-lg-4 text-start">
-                                                <p class="p-0 m-0 text-secondary"> Total Price : $ </p>
-                                                {{ App\Models\Admin\DealSas::where('rfq_id', $rfq_details->id)->value('grand_total') }}
-                                            </div>
-                                            <div class="col-lg-4 text-start">
-                                                <p class="p-0 m-0 text-secondary">Status : </p>
-                                                <span
-                                                    class="text-success p-1">{{ ucfirst($rfq_details->status) }}</span>
-                                            </div>
-                                        </div>
-                                        <div class="row pb-3 px-3">
-                                            <div class="col-lg-4 text-start">
-                                                <p class="p-0 m-0 text-secondary">Assigned Sales Manager (L1) :</p>
-                                                {{ App\Models\User::where('id', $rfq_details->sales_man_id_L1)->value('name') }}
-
-
-                                            </div>
-                                            <div class="col-lg-4 text-start">
-                                                @if ($rfq_details->sales_man_id_T1)
-                                                    <p class="p-0 m-0 text-secondary">Assigned Sales Manager (T1) :</p>
-                                                    {{ App\Models\User::where('id', $rfq_details->sales_man_id_T1)->value('name') }}
-                                                @endif
-                                            </div>
-                                            <div class="col-lg-4 text-start">
-                                                @if ($rfq_details->sales_man_id_T2)
-                                                    <p class="p-0 m-0 text-secondary"> Assigned Sales Manager (T2) :</p>
-                                                    {{ App\Models\User::where('id', $rfq_details->sales_man_id_T2)->value('name') }}
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
     <div id="assign-manager-{{ $rfq->rfq_code }}" class="modal fade" tabindex="-1" style="display: none;"
         aria-hidden="true">
         <div class="modal-dialog modal-dialog-scrollable modal-lg">
@@ -234,7 +118,7 @@
                                                         <table class="table table-bordered table-striped p-1">
                                                             <thead
                                                                 style="background-color: #2472979e !important;color: white !important;">
-                                                                @if (count($rfq->rfqProducts) > 0)
+                                                                @if ($rfq->rfqProducts->count() > 0)
                                                                     <tr>
                                                                         <th class="p-1"> Product Name</th>
                                                                         <th class="p-1"> Quantity </th>
@@ -251,60 +135,35 @@
                                                             </thead>
                                                         </table>
                                                     </div>
-                                                    <div class="row pb-3 px-3">
+                                                    {{-- <div class="row pb-3 px-3">
                                                         <div class="col-lg-4 text-start">
                                                             <p class="p-0 m-0 text-secondary">Assigned Sales Manager
                                                                 (L1) :</p>
-                                                            {{ App\Models\User::where('id', $rfq_details->sales_man_id_L1)->value('name') }}
+                                                            {{ App\Models\User::where('id', $rfq->sales_man_id_L1)->value('name') }}
 
 
                                                         </div>
                                                         <div class="col-lg-4 text-start">
-                                                            @if ($rfq_details->sales_man_id_T1)
+                                                            @if ($rfq->sales_man_id_T1)
                                                                 <p class="p-0 m-0 text-secondary">Assigned Sales
                                                                     Manager (T1) :</p>
-                                                                {{ App\Models\User::where('id', $rfq_details->sales_man_id_T1)->value('name') }}
+                                                                {{ App\Models\User::where('id', $rfq->sales_man_id_T1)->value('name') }}
                                                             @endif
                                                         </div>
                                                         <div class="col-lg-4 text-start">
-                                                            @if ($rfq_details->sales_man_id_T2)
+                                                            @if ($rfq->sales_man_id_T2)
                                                                 <p class="p-0 m-0 text-secondary"> Assigned Sales
                                                                     Manager (T2) :</p>
-                                                                {{ App\Models\User::where('id', $rfq_details->sales_man_id_T2)->value('name') }}
+                                                                {{ App\Models\User::where('id', $rfq->sales_man_id_T2)->value('name') }}
                                                             @endif
                                                         </div>
-                                                    </div>
+                                                    </div> --}}
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
 
-                                <table class="table border p-1">
-                                    <tbody>
-                                        
-                                        <tr>
-                                            <td colspan="2">
-                                                Assigned Sales Manager (L1) :
-                                                {{ App\Models\User::where('id', $rfq->sales_man_id_L1)->value('name') }}
-                                                <br>
-                                                @if ($rfq->sales_man_id_T1)
-                                                    Assigned Sales Manager (T1) :
-                                                    {{ App\Models\User::where('id', $rfq->sales_man_id_T1)->value('name') }}
-                                                    <br>
-                                                @endif
-                                                @if ($rfq->sales_man_id_T2)
-                                                    Assigned Sales Manager (T2) :
-                                                    {{ App\Models\User::where('id', $rfq->sales_man_id_T2)->value('name') }}
-                                                @endif
-                                            </td>
-                                            <td>
-                                                Status :
-                                                <span class="badge bg-success p-1">{{ ucfirst($rfq->status) }}</span>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
                                 <div class="row p-2">
                                     <div class="col-12 text-center">
                                         <strong>Assign Sales Manager :</strong>
