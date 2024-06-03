09098a06f2f3486f25b06254dca3e21d375c4b27
Sazeduzzaman <sazeduzzaman@yahoo.com>
2024-06-03 11:52:32 +0600

    Some Changes Done

--- resources/views/frontend/quotaion/link.blade.php
+++ resources/views/frontend/quotaion/link.blade.php
@@ -1,10 +1,19 @@
 @extends('frontend.master')
+
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row py-5 pt-3">
             <div class="col-lg-6 offset-lg-3 mx-auto">
-                <div>
-                    <h1>Quotation Againest Your RFQ</h1>
+                <div class="text-center main_color">
+                    <h3>Quotation Against Your RFQ</h3>
+                </div>
+                <div class="pt-3">
+                    <iframe src="https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/44678.pdf"
+                        class="w-100 border" height="768px">
+                    </iframe>
+                </div>
+                <div class="d-flex justify-content-center my-5">
+                    <a class="btn-color" href="https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/44678.pdf" download>Download A Quotation</a>
                 </div>
             </div>
         </div>
