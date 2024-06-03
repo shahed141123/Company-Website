63aee1fd905a0d4fb2c7ec3c00cdf6a50a531676
Sazeduzzaman <sazeduzzaman@yahoo.com>
2024-06-03 11:30:10 +0600

    Some Changes Done

--- resources/views/frontend/quotaion/link.blade.php
+++ resources/views/frontend/quotaion/link.blade.php
@@ -1,5 +1,12 @@
 @extends('frontend.master')
 @section('content')
-
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3 mx-auto">
+                <div>
+                    <h1>Quotation Againest Your RFQ</h1>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
-
