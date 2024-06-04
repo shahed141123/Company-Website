827d5fe3c448f3715a229d019a92a9f827a55ca6
Sazeduzzaman <sazeduzzaman@yahoo.com>
2024-06-04 11:19:19 +0600

    Some Changes Done

--- resources/views/pdf/quotation.blade.php
+++ resources/views/pdf/quotation.blade.php
@@ -70,7 +70,7 @@
                 <th width="36%"></th>
                 <th class="p-3" width="32%">
                     <p class="text-start"
-                        style="font-size: 2em;font-weight: 600;margin-bottom: 0;color: #fff; padding: 0px 18px !important;">
+                        style="font-size: 2em;font-weight: 600;margin-bottom: 0;color: #fff; padding: 0px 6px !important;">
                         {{ $quotation->quotation_title }}
                     </p>
                 </th>
@@ -111,151 +111,186 @@
                 </tr>
             </tbody>
         </table>
+        <div class="mx-4">
+            <table class="table-responsive">
+                <thead>
+                    <tr style="background-color: #e5e5e5;color: #3d3d3d; border: 1px solid #eee;font-size: 13px;">
+                        <th width="6%" style="text-align: center;padding: 0.5rem;font-weight: 400;">
+                            SL</th>
+                        <th width="49%" style="text-align: center;padding: 0.5rem;font-weight: 400;">
+                            Product Description</th>
+                        <th width="8%" style="text-align: center;padding: 0.5rem;font-weight: 400;">
+                            Qty</th>
+                        <th width="15%" style="text-align: center;padding: 0.5rem;font-weight: 400;">
+                            Unit Price (<span class="currency">TK</span>)</th>
+                        <th width="15%" style="text-align: center;padding: 0.5rem;font-weight: 400;">
+                            Total (<span class="currency">TK</span>)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="text-center">1</td>
+                        <td>
+                            <p class="mb-0 p-2">
+                                Acronis Access - subscription license (annual) - 1 user
+                            </p>
+                        </td>
+                        <td class="text-center">
+                            <p class="mb-0 p-2">
+                                1
+                            </p>
+                        </td>
 
-        <table class="table-responsive">
+                        <td class="text-center">
+                            <p class="mb-0 p-2">
+                                393
+                            </p>
+                        </td>
+                        <td class="text-center">
+                            <p class="mb-0 p-2">
+                                393
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-center">1</td>
+                        <td>
+                            <p class="mb-0 p-2">
+                                Acronis Access - subscription license (annual) - 1 user
+                            </p>
+                        </td>
+                        <td class="text-center">
+                            <p class="mb-0 p-2">
+                                1
+                            </p>
+                        </td>
+
+                        <td class="text-center">
+                            <p class="mb-0 p-2">
+                                393
+                            </p>
+                        </td>
+                        <td class="text-center">
+                            <p class="mb-0 p-2">
+                                393
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-center">1</td>
+                        <td>
+                            <p class="mb-0 p-2">
+                                Acronis Access - subscription license (annual) - 1 user
+                            </p>
+                        </td>
+                        <td class="text-center">
+                            <p class="mb-0 p-2">
+                                1
+                            </p>
+                        </td>
+
+                        <td class="text-center">
+                            <p class="mb-0 p-2">
+                                393
+                            </p>
+                        </td>
+                        <td class="text-center">
+                            <p class="mb-0 p-2">
+                                393
+                            </p>
+                        </td>
+                    </tr>
+                    <tr style="border-top: 2px solid #eee;">
+                        <td colspan="4">
+                            <p class="mb-0 p-2 text-end fw-bolder">
+                                Sub Total
+                            </p>
+                        </td>
+                        <td class="text-center">
+                            <p class="mb-0 p-2 fw-bolder">
+                                Tk <span>552</span>
+                            </p>
+                        </td>
+                    </tr>
+                    <tr style="background-color: #eee;">
+                        <td colspan="4">
+                            <p class="mb-0 p-2 text-end fw-bolder">
+                                Grand Total
+                            </p>
+                        </td>
+                        <td class="text-center">
+                            <p class="mb-0 p-2 fw-bolder">
+                                Tk <span>552</span>
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="mx-4">
+            <table class="table-responsive w-100 mt-5">
+                <thead style="background-color: #e5e7eb;">
+                    <tr>
+                        <th colspan="2" class="header-title text-center p-2">
+                            Terms & Conditions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td width="15%" class="p-2">
+                            Validate:
+                        </td>
+                        <td width="85%" class="p-2">
+                            Provident eu qui ea
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="15%" class="p-2">
+                            Validate:
+                        </td>
+                        <td width="85%" class="p-2">
+                            Provident eu qui ea
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <table class="table-responsive w-100 borde-0 mx-4 my-5">
             <thead>
-                <tr style="background-color: #e5e5e5;color: #3d3d3d; border: 1px solid #eee;font-size: 13px;">
-                    <th width="6%" style="text-align: center;padding: 0.5rem;font-weight: 400;">
-                        SL</th>
-                    <th width="49%" style="text-align: center;padding: 0.5rem;font-weight: 400;">
-                        Product Description</th>
-                    <th width="8%" style="text-align: center;padding: 0.5rem;font-weight: 400;">
-                        Qty</th>
-                    <th width="15%" style="text-align: center;padding: 0.5rem;font-weight: 400;">
-                        Unit Price (<span class="currency">TK</span>)</th>
-                    <th width="15%" style="text-align: center;padding: 0.5rem;font-weight: 400;">
-                        Total (<span class="currency">TK</span>)</th>
-                </tr>
-            </thead>
-            <tbody>
                 <tr>
-                    <td class="text-center">1</td>
-                    <td>
-                        <p class="mb-0 p-2">
-                            Acronis Access - subscription license (annual) - 1 user
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p class="mb-0 p-2">
-                            1
-                        </p>
-                    </td>
-
-                    <td class="text-center">
-                        <p class="mb-0 p-2">
-                            393
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p class="mb-0 p-2">
-                            393
-                        </p>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="text-center">1</td>
-                    <td>
-                        <p class="mb-0 p-2">
-                            Acronis Access - subscription license (annual) - 1 user
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p class="mb-0 p-2">
-                            1
-                        </p>
-                    </td>
-
-                    <td class="text-center">
-                        <p class="mb-0 p-2">
-                            393
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p class="mb-0 p-2">
-                            393
-                        </p>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="text-center">1</td>
-                    <td>
-                        <p class="mb-0 p-2">
-                            Acronis Access - subscription license (annual) - 1 user
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p class="mb-0 p-2">
-                            1
-                        </p>
-                    </td>
-
-                    <td class="text-center">
-                        <p class="mb-0 p-2">
-                            393
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p class="mb-0 p-2">
-                            393
-                        </p>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="4">
-                        <p class="mb-0 p-2 text-end fw-bolder">
-                            Sub Total
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p class="mb-0 p-2 fw-bolder">
-                            Tk <span>552</span>
-                        </p>
-                    </td>
-                </tr>
-                <tr style="background-color: #eee;">
-                    <td colspan="4">
-                        <p class="mb-0 p-2 text-end fw-bolder">
-                            Grand Total
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p class="mb-0 p-2 fw-bolder">
-                            Tk <span>552</span>
-                        </p>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-
-
-        <table class="table-responsive w-100 mx-4 pdf-header-info">
-            <thead style="background-color: #e5e7eb;">
-                <tr>
-                    <th colspan="2" class="header-title text-center">
-                        Terms & Conditions
+                    <th class="header-title" width="32%">
+                        Thank You
+                    </th>
+                    <th width="36%"></th>
+                    <th class="header-title" width="32%">
+                        sales@ngenitltd.com
                     </th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td width="15%">
-                        Validate:
-                    </td>
-                    <td width="85%">
-                        Provident eu qui ea
-                    </td>
+                    <td>Kawsar Khan</td>
+                    <td></td>
+                    <td>+880 156845986</td>
                 </tr>
                 <tr>
-                    <td width="15%">
-                        Validate:
-                    </td>
-                    <td width="85%">
-                        Provident eu qui ea
-                    </td>
+                    <td>Manager, Business</td>
+                    <td></td>
+                    <td>+880 156845987</td>
                 </tr>
             </tbody>
         </table>
-
+        <table class="pdf-header table-responsive w-100 borde-0">
+            <thead>
+                <th class="p-3">
+                    <p class="text-center"
+                        style="font-size: 1em;font-weight: 600;margin-bottom: 0;color: #fff; padding: 0px 6px !important;">
+                        www.ngenitltd.com
+                    </p>
+                </th>
+            </thead>
+        </table>
     </div>
 </body>
 
